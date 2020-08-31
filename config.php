690ad9e9a314451a710a182fea5a3c20ee3d86a6
NelsonEspadoto <nelsonespadoto@gmail.com>
2020-08-31 16:38:19 -0300

    Alterando Config

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $base = "http://localhost/devsbook";
@@ -10,16 +9,3 @@ $db_pass = "";
 
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 
-=======
-<?php
-session_start();
-$base = "http://localhost/devsbook";
-
-$db_name = "devsbook";
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-
-$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
-
->>>>>>> d59d33e0d47727acc668a1d5129e2df46371cc8b
