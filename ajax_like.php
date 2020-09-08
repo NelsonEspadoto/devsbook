deb9a60ff52a9bf23e189a9f0ea10dd4def8da64
NelsonEspadoto <nelsonespadoto@gmail.com>
2020-09-08 11:49:08 -0300

    atualizando e corrigindo problema do like

--- ajax_like.php
+++ ajax_like.php
@@ -8,10 +8,6 @@ $userInfo = $auth->checkToken();
 
 $id = filter_input(INPUT_GET, 'id');
 
-echo $id; //corrigir aqui
-
-exit;
-
 if (!empty($id)) {
     $postLikeDao = new PostLikeDaoMysql($pdo);
     $postLikeDao->likeToggle($id, $userInfo->id);
